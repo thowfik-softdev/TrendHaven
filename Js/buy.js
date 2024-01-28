9598230dc73d903993c7abc287afcf0c6768976b
Thowfik-mtj3 <thowfikmtj3@gmail.com>
2024-01-28 23:36:53 +0530

    First Commit

---
+++ Js/buy.js
@@ -0,0 +1,15 @@
+var mainImg = document.getElementById("mainImg");
+var smallImg = document.getElementsByClassName("sub_img_image");
+
+smallImg[0].onclick = function () {
+  mainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  mainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  mainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  mainImg.src = smallImg[3].src;
+};
